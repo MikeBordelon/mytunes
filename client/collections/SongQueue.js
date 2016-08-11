a06c2f2d7a265a3e2eab0ab3b8704f9953f96dc4
MikeBordelon <374mike@gmail.com>
2016-08-10 18:46:28 -0700

    Moving workstations

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,35 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
+
+ 
+  playFirst: function(url) {
+    console.log(this.length)
+    return url;
+  },
+
+  // this.on('all', function(e) {
+  //   if (e === add) {
+  //     this.playFirst(e.url);      
+  //   }
+    
+  // });
+
+  // add: function(song) {
+  //   this.
+  //   if (this.length === 1) {
+  //     return this.playFirst(song.url);  
+  //   } 
+  // },
+
   initialize: function() {
-  }
+    this.on('add', function(e) {
+      console.log(e);
+      if (this.length === 1) {
+        return this.playFirst(e.url);  
+      }   
+    });
+  },
+
 
 });
